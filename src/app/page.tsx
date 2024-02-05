486f4165dd2a513fa3a4168ed8acb73a8013f8f8
Edward Smith Silvia <esmithsilvia@gmail.com>
2024-02-04 20:04:08 -0500

    home page style testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,83 @@
 export default function Home() {
-    return <h1>Home</h1>
+    return (
+        <div>
+        <div
+            data-wf-domain="resto-archive.webflow.io"
+            data-wf-page="65b85442836d53a14885f94c"
+            data-wf-site="65b85442836d53a14885f949"
+        />
+
+        <head>
+            <meta charSet="utf-8" />
+            <title>Resto-archive</title>
+            <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <meta content="Webflow" name="generator" />
+            <link href="./variables.module.scss" rel="stylesheet" type="text/scss" />
+            {/* <script type="text/javascript">
+                                        !function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);
+                                    </script> */}
+        </head>
+
+        <body className="body">
+            <div className="nav">
+            <img
+                src="https://assets-global.website-files.com/65b85442836d53a14885f949/65b856f80996f4161a95f19b_933310-200.png"
+                loading="eager"
+                alt=""
+                className="menu"
+            />
+            <img
+                src="https://assets-global.website-files.com/65b85442836d53a14885f949/65b8569b8489a104c80496be_ar-or-ra-letter-logo-design-vector.jpg"
+                loading="lazy"
+                alt=""
+                className="logo"
+            />
+            <img
+                src="https://assets-global.website-files.com/65b85442836d53a14885f949/65b858316581881f091ff7d1_875351-200.png"
+                loading="lazy"
+                alt=""
+                className="search"
+            />
+            </div>
+            <div className="container">
+            <h1 className="heading">Resto-Archive</h1>
+            <h1
+                data-w-id="c1d4af52-d0ac-61da-0743-0c142fce963b"
+                className="heading-2"
+                style={{ opacity: 1 }}
+            >
+                Preserve The Past
+            </h1>
+            </div>
+            <img
+            src="https://assets-global.website-files.com/65b85442836d53a14885f949/65b87306d124829fb4d3b8be_45098456.jpg"
+            loading="lazy"
+            data-w-id="c9edce9d-174a-3622-0438-c223ec1e63e8"
+            sizes="(max-width: 1920px) 100vw, 1920px"
+            alt=""
+            srcSet="https://assets-global.website-files.com/65b85442836d53a14885f949/65b87306d124829fb4d3b8be_45098456-p-500.jpg 500w,
+                            https://assets-global.website-files.com/65b85442836d53a14885f949/65b87306d124829fb4d3b8be_45098456-p-800.jpg 800w,
+                            https://assets-global.website-files.com/65b85442836d53a14885f949/65b87306d124829fb4d3b8be_45098456-p-1080.jpg 1080w,
+                                https://assets-global.website-files.com/65b85442836d53a14885f949/65b87306d124829fb4d3b8be_45098456-p-1600.jpg 1600w,
+                                https://assets-global.website-files.com/65b85442836d53a14885f949/65b87306d124829fb4d3b8be_45098456.jpg 1920w"
+            className="image-2"
+            style={{ opacity: 1 }}
+            />
+            <blockquote
+            data-w-id="fdc290a4-2a4e-b0b2-4e4f-5dfb50faffee"
+            className="block-quote"
+            style={{ opacity: 1 }}
+            >
+            Search, Save, and Document research and progress of your restorations
+            </blockquote>
+            <script
+            src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=65b85442836d53a14885f949"
+            type="text/javascript"
+            integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+            crossOrigin="anonymous"
+            />
+            <script src="./script.jsx" type="text/javascript" />
+        </body>
+        </div>
+    );
 }
